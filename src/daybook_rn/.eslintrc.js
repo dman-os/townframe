c06c6d8627092df2d68f5ff0cd00e650baed683f
dman-os <61868957+dman-os@users.noreply.github.com>
2026-04-09 14:23:01 +0300

    fix: 90m cap on jobs

--- src/daybook_rn/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-// https://docs.expo.dev/guides/using-eslint/
-module.exports = {
-  ignorePatterns: ["/dist/*"],
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error",
-  },
-};
