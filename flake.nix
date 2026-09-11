98090260c12d5ac18e8a616acac74b58aa5d01c0
dman-os <61868957+dman-os@users.noreply.github.com>
2026-09-11 07:25:42 +0300

    refactor: split out cli e2e

--- flake.nix
+++ flake.nix
@@ -60,8 +60,8 @@
 
           ghjkMainEnv = {
             CARGO_BUILD_JOBS = "8";
-            # CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "clang";
-            # CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+            CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.clang}/bin/clang";
+            CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS = "-C link-arg=-fuse-ld=${pkgs.mold-wrapped}/bin/mold";
           };
 
           ghjkDevEnv = {
